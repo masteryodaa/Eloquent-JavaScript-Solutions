c1434d140e3b7dbef5f6bf0e15fbab3633501537
Ujjwal Kumar <kujjwal42@gmail.com>
2021-09-26 01:40:42 +0530

    list

---
+++ 4. Data Structure - Objects and Arrays/3. A List.js
@@ -0,0 +1,7 @@
+// A List
+
+/* Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth. */
+
+
