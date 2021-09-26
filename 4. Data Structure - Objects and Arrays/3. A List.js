9bbc70b07a5eb892327292908e7d6eca798dd819
Ujjwal Kumar <kujjwal42@gmail.com>
2021-09-26 22:49:48 +0530

    nth

--- 4. Data Structure - Objects and Arrays/3. A List.js
+++ 4. Data Structure - Objects and Arrays/3. A List.js
@@ -5,6 +5,7 @@
 If you haven’t already, also write a recursive version of nth. */
 
 
+
 let list = null;
 
 function arrayToList(array){
@@ -24,9 +25,58 @@ function arrayToList(array){
 
 }
 
-console.log(arrayToList([10,20,30,40]));
+// console.log(arrayToList([10,20,30,40]));
 // → {value: 10, rest: {value: 20, rest: null}}
 
 
 
 
+
+let arr = []
+function listToArray(object){
+    
+
+    if(object.rest==null){
+
+        arr.push(object.value);
+        return arr;
+    }
+    else{
+        arr.push(object.value, object.rest.value);
+        return listToArray(object.rest)
+    }
+}
+
+// console.log(listToArray(arrayToList([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])));
+// → [10, 20, 30]
+
+
+
+
+
+function prepend(value, rest){
+    return {
+        value:value,
+        rest:rest
+    }
+}
+
+// console.log(prepend(10, prepend(20, prepend(30, null))));
+
+
+
+
+
+function nth(object, position){
+    if (object==null) {
+        return undefined;
+    } else if (position==0){
+        return object.value;
+    } else{
+        return nth(object.rest, position-1);
+    }
+}
+
+console.log(arrayToList([10, 20, 30]))
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
